5ea40aff582d07fdad5dca2fbb2961eb125bbe94
inder <indertejus@gmail.com>
2024-07-17 18:34:54 +0530

    Removed nvim, updated Windows Powershell

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,1 @@
-# Invoke-Expression (&starship init powershell)
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/bubbles.omp.json"| Invoke-Expression
--- nvim/README.txt
+++
@@ -1,1 +0,0 @@
-THIS CONFIG IS NOT MINE. The original config is at https://github.com/mister-coded/dotfiles.
